9dfb672af05717dcd9a1bc8b5cb9aca4c94f3c66
Molefe Skosana <molefe.jumpstart@gmail.com>
2016-03-07 10:29:43 +0200

    Initial database login.

---
+++ jdbcdemo/src/jdbcdemo/Driver.java
@@ -0,0 +1,27 @@
+package jdbcdemo;
+
+import java.sql.*;
+public class Driver {
+
+	public static void main(String[] args) {
+		try{
+			// 1.Get a connection to a database 
+				Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/churchdb","root","molefe");
+			// 2.Create a statement.
+				Statement myStmt = myConn.createStatement();
+				
+			// 3.Execute a SQL query.
+				ResultSet myRs = myStmt.executeQuery("select * from congregantstb;");
+			
+			// 4.Process the result set.
+				while(myRs.next()){
+					System.out.println(myRs.getString("first_name")+ ", " + myRs.getString("last_name")+", "+myRs.getString("birth_date")+", "+myRs.getString("gender")+
+						","+	myRs.getString("contact")+", "+myRs.getString("email")+", "+myRs.getString("address")+", "+myRs.getString("membership_duration")
+						+", "+ myRs.getString("baptism_status"));
+				}
+		}catch(Exception exc){
+			exc.printStackTrace();
+		}
+	}
+
+}
